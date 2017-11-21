420e831fc3e1e3c40e240eff5e1a2196f8159ecf
PY <31885847+qin1980@users.noreply.github.com>
2017-11-21 06:19:39 -0600

    Update myFunction1.js

--- myFunction1.js
+++ myFunction1.js
@@ -2,16 +2,16 @@ function roundX(val, precision) {
   return Math.round(Math.round(val * Math.pow(10, (precision || 0) + 1)) / 10) / Math.pow(10, (precision || 0));
 }
 
-function myFunction1(s1, s2, s3, s4, s5, yr) {
-	var ss1= roundX(s1, 1);
-	var ss2= roundX(s2, 1);
-	var ss3= roundX(s3, 1);
-	var ss4= roundX(s4, 1);
-	var ss5= roundX(s5, 1);
-	var yr=yr.toString() + "年";
+function myFunction1(s1) {
+	var ss1= roundX(s1[0], 1);
+	var ss2= roundX(s1[1], 1);
+	var ss3= roundX(s1[2], 1);
+	var ss4= roundX(s1[3], 1);
+	var ss5= roundX(s1[4], 1);
+	var yr=s1[5].toString() + "年";
 
-    var show = "(" + ss1.toString() + "+" + ss2.toString() + "+" + ss3.toString() + "+" + ss4.toString() + "+" +  s5.toString() + ") = ";
-    var sum = (s1 + s2 + s3 + s4 + s5);
+    var show = "(" + ss1.toString() + "+" + ss2.toString() + "+" + ss3.toString() + "+" + ss4.toString() + "+" +  ss5.toString() + ") = ";
+    var sum = (s1[0] + s1[0] + s1[0]+ s1[0] + s1[0]);
     var ans= round(sum, 2);
     var str = yr + show + ans.toString();
     return str;
