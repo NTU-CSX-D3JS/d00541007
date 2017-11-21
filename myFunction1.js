087a413d9208f9f09064c72c513534c68115f7c4
PY <31885847+qin1980@users.noreply.github.com>
2017-11-21 05:43:35 -0600

    Update myFunction1.js

--- myFunction1.js
+++ myFunction1.js
@@ -4,7 +4,7 @@ function myFunction1(s1, s2, s3, s4, s5, yr) {
 	var ss3= roundX(s3, 1)
 	var ss4= roundX(s4, 1)
 	var ss5= roundX(s5, 1)
-	var yr=yr.toString + "年"
+	var yr=yr.toString() + "年"
 
     var show = "(" + ss1.toString() + "+" + ss2.toString() + "+" + ss3.toString() + "+" + ss4.toString() + "+" +  s5.toString() + ") = ";
     var sum = (s1 + s2 + s3 + s4 + s5);
