618471196e7223cc372436b1aac2a63b9e351e03
PY <31885847+qin1980@users.noreply.github.com>
2017-11-21 05:48:16 -0600

    Update myFunction1.js

--- myFunction1.js
+++ myFunction1.js
@@ -1,14 +1,14 @@
 function myFunction1(s1, s2, s3, s4, s5, yr) {
-	var ss1= roundX(s1, 1)
-	var ss2= roundX(s2, 1)
-	var ss3= roundX(s3, 1)
-	var ss4= roundX(s4, 1)
-	var ss5= roundX(s5, 1)
+	var ss1= round(s1, 1)
+	var ss2= round(s2, 1)
+	var ss3= round(s3, 1)
+	var ss4= round(s4, 1)
+	var ss5= round(s5, 1)
 	var yr=yr.toString() + "年"
 
     var show = "(" + ss1.toString() + "+" + ss2.toString() + "+" + ss3.toString() + "+" + ss4.toString() + "+" +  s5.toString() + ") = ";
     var sum = (s1 + s2 + s3 + s4 + s5);
-    var ans= roundX(sum, 2)
+    var ans= round(sum, 2)
     var str = yr + show + ans.toString();
     return str
 }
