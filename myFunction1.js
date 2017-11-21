0bba7fc55ed0caffa55081c92eea4ac3f0bb2f74
PY <31885847+qin1980@users.noreply.github.com>
2017-11-21 06:51:42 -0600

    Update myFunction1.js

--- myFunction1.js
+++ myFunction1.js
@@ -3,15 +3,16 @@ function roundX(val, precision) {
 }
 
 function myFunction1(s1) {
-	var ss1= roundX(s1[0], 1);
-	var ss2= roundX(s1[1], 1);
-	var ss3= roundX(s1[2], 1);
-	var ss4= roundX(s1[3], 1);
-	var ss5= roundX(s1[4], 1);
-	var yr=s1[5].toString() + " 年 總電力消費量為";
+	var yr=s1[0].toString() + " 年 總電力消費量為";
+	var ss1= roundX(s1[1], 1);
+	var ss2= roundX(s1[2], 1);
+	var ss3= roundX(s1[3], 1);
+	var ss4= roundX(s1[4], 1);
+	var ss5= roundX(s1[5], 1);
+	
 
     var show = "(" + ss1.toString() + "+" + ss2.toString() + "+" + ss3.toString() + "+" + ss4.toString() + "+" +  ss5.toString() + ") = ";
-    var sum = (s1[0] + s1[1] + s1[2]+ s1[3] + s1[4]);
+    var sum = (s1[1] + s1[2] + s1[3]+ s1[4] + s1[5]);
     var ans= roundX(sum, 2);
     var str = yr + show + ans.toString();
     return str;
